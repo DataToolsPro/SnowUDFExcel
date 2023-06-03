1ae851f9b522a108b6ca989252dbb4c1cc5ebc09
rmgoodm <ryan@goodmangroupllc.com>
2023-06-02 21:52:04 -0700

    Update NETWORKDAYS.sql
    Fixed a bug that was not calculating negative networkdays

--- NETWORKDAYS.sql
+++ NETWORKDAYS.sql
@@ -1,54 +1,58 @@
 CREATE OR REPLACE FUNCTION networkdays(START_DATE DATE, END_DATE DATE, HOLIDAYS VARCHAR(1000000))
-RETURNS INT
+RETURNS VARCHAR
 LANGUAGE JAVASCRIPT
 AS $$
-    // Checking if any of the input parameters are null
-    // If any parameter is null, the function returns null
+    // Check for null input and return null if any argument is null
     if(START_DATE == null || END_DATE == null || HOLIDAYS == null){
         return null;
     }
-
-    // Creating JavaScript Date objects from the input parameters
+    
+    // Convert the START_DATE and END_DATE to JavaScript Date objects
     var start_date = new Date(START_DATE);
     var end_date = new Date(END_DATE);
 
-    // Splitting the HOLIDAYS string by commas into an array
-    // Then creating a Set from the array for faster lookup
+    // Split the HOLIDAYS string by commas into an array
     var holidays_list = HOLIDAYS.split(',');
-    var holiday_set = new Set(holidays_list);
 
-    // This will hold the count of working days
+    // Initialize a counter for the number of workdays
     var days_int = 0;
 
-    // Function to format a date as 'yyyy-mm-dd'
-    // This is needed for comparing dates in the loop
+    // Initialize a variable to determine if we should increment or decrement the workday count
+    // By default, we will increment the count
+    var increment = 1;
+
+    // If end_date is before start_date, swap them and set increment to -1 to decrement the workday count
+    if(end_date < start_date){
+        var temp_date = start_date;
+        start_date = end_date;
+        end_date = temp_date;
+        increment = -1;
+    }
+
+    // Function to format dates as 'YYYY-MM-DD' to match with holidays list
     function formatDate(d) {
         var month = '' + (d.getMonth() + 1),
             day = '' + d.getDate(),
             year = d.getFullYear();
 
-        // Padding month and day with leading zeroes if needed
         if (month.length < 2) 
             month = '0' + month;
         if (day.length < 2) 
             day = '0' + day;
 
-        // Returning the formatted date string
         return [year, month, day].join('-');
     }
 
-    // Looping from start_date to end_date
+    // Iterate through each date between start_date and end_date
     for(var curr_date = start_date; curr_date <= end_date; curr_date.setDate(curr_date.getDate()+1)) {
-        // Formatting curr_date as 'yyyy-mm-dd'
         var curr_date_str = formatDate(curr_date);
         
-        // Checking if curr_date is not a weekend and not a holiday
-        // If both conditions are true, increment days_int
-        if(curr_date.getUTCDay() % 6 !== 0 && !holiday_set.has(curr_date_str)) {
-            days_int++;
+        // If the current day is a weekday and not a holiday, increment or decrement the workday count
+        if(curr_date.getUTCDay() % 6 !== 0 && !holidays_list.includes(curr_date_str)) {
+            days_int += increment;
         }
     }
 
-    // Returning the count of working days
+    // Return the workday count
     return days_int;
 $$;
